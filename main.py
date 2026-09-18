124bfa7756d2ad354c1e4fb60c0e04bf33f6dd7e
Av1xo <rostyslav.2004.zhurbenko@gmail.com>
2026-09-18 03:24:12 +0300

    (feat) Add path checker

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from sys import argv
 from utils import load_data, check_path
 
+def get_data_from_path(args: list[str]) -> list[str]:
+    data_path: str | None = args[1] if len(args) > 1 else None
+
+    while(True):
+        if data_path is None:
+            data_path = input("Please, enter path to data file -> ")
+        
+        path, ok = check_path(data_path)
+        
+        if ok:
+            return load_data(path)
 
 def main(args: list[str]) -> None:
-    if len(args) > 1:
-        data_path = args[1]
-    else:
-        data_path = input("Please, enter path to data file -> ")
-        
-    data = load_data()
-    
+    data: list[str] = get_data_from_path(args)
+
+
 if __name__ == "__main__":
     main(argv)
--- utils.py
+++ utils.py
@@ -1,5 +1,14 @@
+from pathlib import Path
+
+
+def check_path(in_path: str) -> tuple[Path | None, bool]:
+    path: Path = Path(in_path)
+    
+    if path.is_file() and path.suffix == ".txt":
+        return path, True
+    
+    return None, False
+
+
 def load_data():
     pass
-
-def check_path(path: str):
-    pass 
